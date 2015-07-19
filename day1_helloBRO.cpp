22cd210e6329d23dfbdfc7bf319961bf2a5ed8f7
klynx <knight.lynx@ya.ru>
2015-07-19 17:21:01 +0000

    Update day1_helloBRO.cpp

--- day1_helloBRO.cpp
+++ day1_helloBRO.cpp
@@ -5,3 +5,12 @@ int main()
 	cout<<"Привет, Бро!"<<endl;
 	return 1;
 }
+/*
+Компилировал с помощью утилиты с++ на linux. изменил урок на сайте интуит
+#include <iostream.h>
+int main()
+{
+   std::cout << "Hello, world!" << std::endl;
+   return 1;
+}
+*/
